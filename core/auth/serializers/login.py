4e9d9e26c437c917eb581e457e60f700948e5375
orlyplaza <plaza.orly.omeles@gmail.com>
2024-02-16 08:13:43 +0800

    docs: Added comment in the like 14

---
+++ core/auth/serializers/login.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import update_last_login
+from rest_framework_simplejwt.settings import api_settings
+from rest_framework_simplejwt.serializers import TokenObtainSerializer
+
+
+class LoginSerializer(TokenObtainSerializer):
+    def validate(self, attrs):
+        """Validating the request data before obtaining the refresh and access token from the inherited class."""
+        data = super().validate(attrs)
+        refresh = self.get_token(self.user)
+        data["refresh"] = str(refresh)
+        data["access"] = str(refresh.access_token)
+
+        # Update last timestamps of the User
+        if api_settings.UPDATE_LAST_LOGIN:
+            update_last_login(None, self.user)
+        return data
