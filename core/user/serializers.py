5f0c9e88c6d00b56c3e5a22263bc745f110b91b4
orlyplaza <plaza.orly.omeles@gmail.com>
2024-02-17 15:02:02 +0800

    feat: add user serializer

---
+++ core/user/serializers.py
@@ -0,0 +1,17 @@
+from . models import User
+from core.abstract.serializers import AbstractSerializer
+
+
+class UserSerializer(AbstractSerializer):
+    class Meta:
+        """List of all fields that will be included in request or a response."""
+        model = User
+        fields = [
+            "id",
+            "email",
+            "username",
+            "is_active",
+            "created_at",
+            "updated_at",
+        ]
+        read_only_fields = ["is_active"]
