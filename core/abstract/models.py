817a89f0c369de338526412995ce16ffa55a493e
orlyplaza <plaza.orly.omeles@gmail.com>
2024-02-17 15:02:23 +0800

    feat(!): change timestamp field names

--- core/abstract/models.py
+++ core/abstract/models.py
@@ -19,8 +19,8 @@ class AbstractModel(models.Model):
     public_id = models.UUIDField(
         db_index=True, unique=True, default=uuid.uuid4, editable=False
     )
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     objects = AbstractManager()
 
--- core/abstract/serializers.py
+++ core/abstract/serializers.py
@@ -4,5 +4,5 @@ from rest_framework import serializers
 class AbstractSerializer(serializers.ModelSerializer):
     """Every serializer contains id, created and updated fields."""
     id = serializers.UUIDField(source="public_id", read_only=True, format="hex")
-    created = serializers.DateTimeField(read_only=True)
-    updated = serializers.DateTimeField(read_only=True)
+    created_at = serializers.DateTimeField(read_only=True)
+    updated_at = serializers.DateTimeField(read_only=True)
