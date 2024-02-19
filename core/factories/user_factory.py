61100ab149a0357a8d98c0dc4ff6f1d2026db2a2
orlyplaza <plaza.orly.omeles@gmail.com>
2024-02-19 23:17:59 +0800

    feat: added UserFactory for testing

---
+++ core/factories/user_factory.py
@@ -0,0 +1,15 @@
+import factory
+from core.user.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Faker("user_name")
+    email = factory.Faker("email")
+    password = factory.PostGenerationMethodCall('set_password', 'testpassword')
+
+    @classmethod
+    def create_superuser(cls, **kwargs):
+        return cls(is_staff=True, is_superuser=True, **kwargs)
