95b02c2c9c97127d663d0c6ffdfc56e0ae300455
orlyplaza <plaza.orly.omeles@gmail.com>
2024-02-19 19:31:41 +0800

    Initial commit

--- core/auth/permissions.py
+++ core/auth/permissions.py
@@ -1,14 +1,19 @@
-from rest_framework.permissions import (
-    BasePermission,
-    SAFE_METHODS
-)
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsAuthenticatedOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        return bool(request.user and request.user.is_authenticated)
+        if request.method in SAFE_METHODS:
+            return bool(request.user and request.user.is_authenticated)
+        elif request.method == "POST":
+            user_public_id = str(request.user.public_id).replace("-", "")
+            author_public_id = request.data.get("author").replace("-", "")
+            return bool(request.user.is_superuser or user_public_id == author_public_id)
+        return False
 
     def has_object_permission(self, request, view, obj):
         if request.method in SAFE_METHODS:
             return True
-        return bool(obj.author == request.user or request.user.is_superuser)
+        elif request.method in ["PUT", "PATCH", "DELETE"]:
+            return bool(request.user.is_superuser or obj.author == request.user)
+        return False
