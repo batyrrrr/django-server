4c7fa475a2ff74946a0f1c18dec18b9bdf2e8b78
orlyplaza <plaza.orly.omeles@gmail.com>
2024-02-19 23:17:03 +0800

    refactor: rename class name from plural to singular

--- core/abstract/viewsets.py
+++ core/abstract/viewsets.py
@@ -2,7 +2,7 @@ from rest_framework import filters
 from rest_framework import viewsets
 
 
-class AbstractViewSets(viewsets.ModelViewSet):
+class AbstractViewSet(viewsets.ModelViewSet):
     """All ViewSets will have the same ordering system."""
     filter_backends =[filters.OrderingFilter]
     ordering_fields = ["updated_at", "created_at"]
