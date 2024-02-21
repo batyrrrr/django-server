1e6ea9b1b12e3affe6d77227cda14bd8fa310240
orlyplaza <plaza.orly.omeles@gmail.com>
2024-02-22 01:42:20 +0800

    ePlease enter the commit message for your changes. Lines starting

--- core/auth/viewsets/login.py
+++ core/auth/viewsets/login.py
@@ -3,7 +3,6 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from rest_framework.permissions import AllowAny
 from core.auth.serializers.login import LoginSerializer
-from rest_framework_simplejwt.exceptions import TokenError, InvalidToken
 
 
 class LoginViewSet(viewsets.ViewSet):
@@ -12,10 +11,10 @@ class LoginViewSet(viewsets.ViewSet):
     serializer_class = LoginSerializer
 
     def create(self, request, *args, **kwargs):
-        """Return the refresh and access token after being validated the request data by the serializer."""
+        """Return the refresh and access token after validating the request data."""
         serializer = self.serializer_class(data=request.data)
-        try:
-            serializer.is_valid(raise_exception=True)
-        except TokenError as error:
-            raise InvalidToken(error.args[0])
-        return Response(serializer.validated_data, status=status.HTTP_200_OK)
+        serializer.is_valid(raise_exception=True)
+        validated_data = serializer.validated_data
+        if validated_data:
+            return Response(validated_data, status=status.HTTP_200_OK)
+        return Response(status=status.HTTP_401_UNAUTHORIZED)
