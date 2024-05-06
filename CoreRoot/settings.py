a94d15df56bd141080ba1290d0bd9a5b51f3e5ac
batyrrrr <bolatbekbatyr41@gmail.com>
2024-05-06 20:09:57 +0500

    first commit

--- CoreRoot/settings.py
+++ CoreRoot/settings.py
@@ -213,5 +213,7 @@ EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = os.environ.get("EMAIL_USE_TLS")
 
+
+
 # Defining a custom user
 AUTH_USER_MODEL = "core_user.User"
